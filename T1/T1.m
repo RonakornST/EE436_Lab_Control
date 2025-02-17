9002f64f88dbe0f20be94cc2105142434d3669e3
RonakornST <119744243+RonakornST@users.noreply.github.com>
2025-02-17 18:38:32 +0700

    finished lab2

--- T1/T1.m
+++ T1/T1.m
@@ -124,3 +124,39 @@ hold off
 ess_Ki03_K4 = refinp(end) - level2(end);
 disp('The value of ess_Ki01_K4 is:');
 disp(ess_Ki03_K4);
+
+%%
+filename = 'T1_lab2_openloop.txt';
+data = readtable(filename, 'FileType', 'text', 'HeaderLines', 3);
+% Extract variables from the table, excluding the last data point
+t = data.Time(1:10000);        % Time data
+refinp = data.ref_inp(1:10000);         % Temperature T1
+level2 = data.lev2(1:10000);         % Temperature T2
+hold on
+plot(t,refinp,'-r', 'LineWidth', 2 )
+plot(t,level2,'-g', 'LineWidth', 2)
+% Shutter position
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Reference input and level in second tanks(Ki=0.3)');
+legend('reference input','level2')
+hold off
+
+%%
+
+%%
+filename = 'T1_lab2_closeloop.txt';
+data = readtable(filename, 'FileType', 'text', 'HeaderLines', 2);
+% Extract variables from the table, excluding the last data point
+t = data.Time(1:end);        % Time data
+refinp = data.ref_inp(1:end);         % Temperature T1
+level2 = data.lev2(1:end);         % Temperature T2
+hold on
+plot(t,refinp,'-r', 'LineWidth', 2 )
+plot(t,level2,'-g', 'LineWidth', 2)
+% Shutter position
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Reference input and level in second tanks(Ki=0.3)');
+legend('reference input','level2')
+hold off
